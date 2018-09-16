845fd7378f4d32835745a893b1faf10669546772
andyztw <zhantu@gmail.com>
2018-09-16 14:21:40 +1200

    added edit and add api

--- src/Component/Layout/Footer.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Typography from "@material-ui/core/Typography";
-
-export default props => (
-  <div>
-    <Typography>Created by Red Star Team - copyrighted 2018</Typography>
-  </div>
-);
