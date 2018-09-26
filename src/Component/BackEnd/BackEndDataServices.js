73dc8bbf02c2a0da3a20f1dcf479b4e07470a224
andyztw <zhantu@gmail.com>
2018-09-27 11:07:11 +1200

    changed url to localhost

--- src/Component/BackEnd/BackEndDataServices.js
+++ src/Component/BackEnd/BackEndDataServices.js
@@ -1,7 +1,7 @@
 
 //path names of domain and api paths to reach backend server
 
-export const url = "https://www.nzbeta.com/";
+export const url = "http://localhost:8900/";
 export const signIn = 'api/v1/users/signin';
 export const addApi = 'api/v1/apis/add_api';
 export const runAll = 'api/v1/apis/run_all';
@@ -9,3 +9,7 @@ export const runSelect = "api/v1/apis/run_selected";
 export const lockInvert = 'api/v1/apis/invert_lock';
 export const editApi = 'api/v1/apis/edit_api'; 
 export const dashBoard = 'api/v1/dashboard';
+
+
+
+
